e9fede14d6b008b27b566aebb4bd2cc4d1a6c0ac
Tío Gallo <juanftp100@gmail.com>
2026-01-19 09:37:50 -0300

    enviroment y option para nuestro servicio

---
+++ src/app/products/services/products.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ProductsResponse } from '../interfaces/product.interface';
+import { Observable, pipe, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+  limit?: number;
+  offset?: number;
+  gender?: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  private http = inject(HttpClient);
+
+  getProducts(options: Options): Observable<ProductsResponse> {
+
+    const { limit = 9, offset = 0, gender = '' } = options;
+
+    return this.http.get<ProductsResponse>(`${baseUrl}/products`, {
+      params: {
+        limit: limit,
+        offset: offset,
+        gender: gender
+      }
+    })
+      .pipe(
+        tap(resp => console.log(resp))
+      );
+  }
+
+}
