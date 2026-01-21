a08cbb5a6b2496a4fd3c7aacd87e1d77e4190d6b
Tío Gallo <juanftp100@gmail.com>
2026-01-21 11:04:57 -0300

    arreglos para el pipe de imagenes de producto

--- src/app/products/pipes/product-image.pipe.ts
+++ src/app/products/pipes/product-image.pipe.ts
@@ -8,15 +8,15 @@ const baseUrl = environment.baseUrl;
 })
 
 export class ProductImagePipe implements PipeTransform {
-  transform(value: string | string[]): any {
+  transform(value: string | string[]): string {
 
     // caso1: array > 1 = primer elemento
-    if ( Array.isArray(value) && value.length > 1 ) {
+    if ( Array.isArray(value) && value.length > 0 && value[0]) {
       return `${baseUrl}/files/product/${value[0]}`;
     }
     // caso2: string
-    if ( value instanceof String && value.trim().length > 0 ) {
-      return `${baseUrl}/files/product/${value}`;;
+    if ( typeof value === 'string' && value.trim().length > 0 ) {
+      return `${baseUrl}/files/product/${value}`;
     }
     // placeholder image: ./assets/images/no-image.jpg
     return `./assets/images/no-image.jpg`;
