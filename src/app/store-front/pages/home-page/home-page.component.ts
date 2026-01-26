03249a0fb15c2c6ef3f7e1ec636d788beb7909ec
Tío Gallo <juanftp100@gmail.com>
2026-01-26 17:13:10 -0300

    paginacion para el home

--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@/products/components/product-card/product-card.component';
 import { ProductsService } from '@/products/services/products.service';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { PaginationComponent } from "@/shared/components/pagination/pagination.component";
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home-page',
@@ -13,10 +15,28 @@ export class HomePageComponent {
 
   productsService = inject(ProductsService);
 
+  activatedRoute = inject(ActivatedRoute);
+
+  // el queryparams puede ser nulo
+  currentPage = toSignal(
+    this.activatedRoute.queryParamMap.pipe(
+      map( params => (params.get('page') ? +params.get('page')! : 1) ),
+      map( page => (isNaN(page) ? 1 : page) ),
+    ),
+    {
+      // siempre va a retornar un valor numérico
+      initialValue: 1,
+    }
+  );
+
   productsResource = rxResource({
-    request: () => ({}),
+    // empieza en 0 por eso restamos 1
+    request: () => ({ page: this.currentPage() - 1 }),
     loader: ( { request } ) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        // traer de 9 en 9
+        offset: request.page * 9
+      });
     }
   });
 
