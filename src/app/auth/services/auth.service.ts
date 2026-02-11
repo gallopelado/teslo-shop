5003bb8a1a62ac704768f109f6cab29b77b56e64
Tío Gallo <juanftp100@gmail.com>
2026-02-11 16:39:45 -0300

    post en autenticacion

---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,55 @@
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { AuthResponse } from '../interfaces/auth-response.interface';
+import { tap } from 'rxjs';
+
+type AuthStatus = 'checking'|'authenticated'|'not-authenticated';
+const baseUrl = environment.baseUrl;
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+
+  private _authStatus = signal<AuthStatus>('checking');
+  private _user = signal<User|null>(null);
+  private _token = signal<string|null>(null);
+
+  private http = inject(HttpClient);
+
+
+  // se hacen computed porque son de lectura
+  // asi evitamos modificaciones de las signals
+  authStatus = computed<AuthStatus>(() => {
+    if(this._authStatus() === 'checking') {
+      return 'checking';
+    }
+    if(this._user()) {
+      return 'authenticated';
+    }
+    return 'not-authenticated';
+  });
+
+  user = computed<User|null>(() => {
+    return this._user();
+  });
+
+  token = computed<string|null>(() => {
+    return this._token();
+  });
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponse>(`${ baseUrl }/auth/login`, {
+      email: email, password: password
+    }).pipe(
+      tap(resp => {
+        this._user.set(resp.user);
+        this._token.set(resp.token);
+        this._authStatus.set('authenticated');
+
+        localStorage.setItem('token', resp.token);
+      }),
+    )
+  }
+
+}
