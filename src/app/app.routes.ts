85ac45023adba0a995f5b7a69c960cb2321cb9a9
Tío Gallo <juanftp100@gmail.com>
2026-02-14 17:06:10 -0300

    uso del canMatch

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from './auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
 
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.routes'),
-    // TODO: Guards
+    canMatch: [
+      // () => {
+      //   console.log('Prohibido!');
+      //   return false;
+      // },
+      NotAuthenticatedGuard,
+    ]
   },
   {
     path: '',
