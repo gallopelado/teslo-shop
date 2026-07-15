7feabb9aec438961465e9f7136e778f746be83a1
Tío Gallo <juanftp100@gmail.com>
2026-07-14 23:24:38 -0300

    formulario producto y botones de selección

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.component.ts
@@ -1,13 +1,59 @@
 import { Product } from '@/products/interfaces/product.interface';
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { ProductCarouselComponent } from "@/products/components/product-card/product-carousel/product-carousel.component";
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '@/utils/form-utils';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarouselComponent],
+  imports: [ProductCarouselComponent, ReactiveFormsModule],
   templateUrl: './product-details.component.html',
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+
   product = input.required<Product>();
+  static slugPattern = '^[a-z0-9_]+(?:-[a-z0-9_]+)*$';
+
+  fb = inject(FormBuilder);
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+  })
+
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    this.productForm.reset(this.product() as any);
+    //this.productForm.patchValue(formLike as any);
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') })
+  }
+
+  onSizeClicked(size: string) {
+    // si no viene nada,entonces se usa un string vacío
+    const currentSizes = this.productForm.value.sizes ?? [];
+    if (currentSizes.includes(size)) {
+      currentSizes.splice(currentSizes.indexOf(size),1);
+    } else {
+      currentSizes.push(size);
+    }
+
+    // actualizamos las tallas
+    this.productForm.patchValue({ sizes: currentSizes });
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
 }
